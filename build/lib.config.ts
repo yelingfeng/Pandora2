c7a7845d7ab343e60a28a90786cc50e5532eef6d
yelingfeng <yelingfeng521@gmail.com>
2022-09-14 13:53:32 +0800

    build(build): change build lib

--- build/lib.config.ts
+++ build/lib.config.ts
@@ -12,11 +12,20 @@ const outputDir = resolve(__dirname, '../lib')
 // rollup 配置
 const rollupOptions = {
   // 确保外部化处理那些你不想打包进库的依赖
-  external: ['vue'],
+  external: [
+    'vue',
+    'dayjs',
+    'element-plus',
+    'lodash-es',
+    '@element-plus/icons-vue'
+  ],
   output: {
     // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
     globals: {
-      vue: 'Vue'
+      vue: 'Vue',
+      'element-plus': 'elementPlus',
+      dayjs: 'dayjs',
+      'lodash-es': 'lodashEs'
     }
   }
 }
@@ -25,7 +34,7 @@ export default defineConfig({
   ...baseConfig,
   publicDir: false,
   build: {
-    // target: 'esnext',
+    target: 'esnext',
     lib: {
       entry: resolve(entryDir, 'index.ts'),
       name: 'pandora2',
@@ -38,7 +47,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     dts({
-      include: ['packages', 'typings']
+      include: ['packages', 'typings', 'global.d.ts']
     })
   ]
 })
---
+++ global.d.ts
@@ -0,0 +1,9 @@
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    PdContainer: typeof import('pandora2')['PdContainer']
+    PdForm: typeof import('pandora2')['PdForm']
+    PdTable: typeof import('pandora2')['PdTable']
+  }
+}
+
+export {}
